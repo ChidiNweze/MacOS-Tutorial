7c342aa434578bec08e3753106c467b3f59b2bd7
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-04 19:55:59 -0400

    Add Filter.swift. Update LandmarkList.swift to allow for filtering.

---
+++ MacLandmarks/Filter.swift
@@ -0,0 +1,21 @@
+//
+//  Filter.swift
+//  MacLandmarks
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-05-04.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import SwiftUI
+
+struct Filter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Filter_Previews: PreviewProvider {
+    static var previews: some View {
+        Filter()
+    }
+}
