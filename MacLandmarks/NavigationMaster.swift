b1be81152abfbb669a6aad90cc17f22517898237
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-05 19:06:46 -0400

    Combine filter and list view. Add CircleImage.swift

--- MacLandmarks/NavigationMaster.swift
+++ MacLandmarks/NavigationMaster.swift
@@ -9,13 +9,29 @@
 import SwiftUI
 
 struct NavigationMaster: View {
+    @Binding var selectedLandmark: Landmark?
+    @State private var filter: FilterType = .all
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Filter(filter: $filter)
+               .controlSize(.small)
+               .padding([.top, .leading], 8)
+               .padding(.trailing, 4)
+            
+            LandmarkList(
+                selectedLandmark: $selectedLandmark,
+                filter: $filter
+            )
+            .listStyle(SidebarListStyle())
+        }
+        .frame(minWidth: 225, maxWidth: 300)
     }
 }
 
 struct NavigationMaster_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationMaster()
+        NavigationMaster(selectedLandmark: .constant(landmarkData[1]))
+            .environmentObject(UserData())
     }
 }
