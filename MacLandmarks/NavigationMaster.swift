5910b75e17c9836ade3fdf401ebb84610a645758
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-04 20:33:05 -0400

    Add NavigationMaster.swift

---
+++ MacLandmarks/NavigationMaster.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationMaster.swift
+//  MacLandmarks
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-05-04.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationMaster: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationMaster_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationMaster()
+    }
+}
